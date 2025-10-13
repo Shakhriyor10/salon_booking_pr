6f16f280cc7e00b2c83b9bdfe1e90a755a2ef6fb
Shakhriyor10 <nnnnafisa@gmail.com>
2025-10-13 12:57:15 +0500

    sadasdasdastating

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,25 @@
+import re
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    phone = forms.CharField(max_length=20, label='Телефон')
+    phone = forms.RegexField(
+        max_length=20,
+        regex=r'^\d{2}-\d{3}-\d{2}-\d{2}$',
+        label='Телефон',
+        error_messages={'invalid': 'Введите номер в формате 93-123-45-67.'}
+    )
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'phone', 'password1', 'password2')
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        digits = re.sub(r"\D", "", phone)
+        return f"+998{digits}"
 
     def save(self, commit=True):
         user = super().save(commit)
@@ -19,4 +31,3 @@ class SignUpForm(UserCreationForm):
         profile.save()
 
         return user
-
