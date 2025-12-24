5b814db5da13ca04515a190bffcd122f5d459890
Shakhriyor10 <77412906+Shakhriyor10@users.noreply.github.com>
2025-12-25 01:26:28 +0500

    Add salon admin application flow

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/',    views.login_view,    name='login'),
     path('logout/',   views.logout_view,   name='logout'),
+    path('salon-admin/apply/', views.salon_admin_application_view, name='salon_admin_apply'),
 ]
